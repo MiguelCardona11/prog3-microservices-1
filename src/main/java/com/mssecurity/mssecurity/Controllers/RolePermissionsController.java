cb17a1bf496c48881cf5a7bd6828f048dfc44b72
MiguelCardona11 <04.migue.cardona@gmail.commit>
2023-09-07 08:45:23 -0500

    creacion de la clase n a n RolePermission

--- src/main/java/com/mssecurity/mssecurity/Controllers/RolePermissionsController.java
+++ src/main/java/com/mssecurity/mssecurity/Controllers/RolePermissionsController.java
@@ -1,4 +1,72 @@
 package com.mssecurity.mssecurity.Controllers;
 
+import com.mssecurity.mssecurity.Models.Permission;
+import com.mssecurity.mssecurity.Models.Role;
+import com.mssecurity.mssecurity.Models.RolePermission;
+import com.mssecurity.mssecurity.Repositories.PermissionRepository;
+import com.mssecurity.mssecurity.Repositories.RolePermissionRepository;
+import com.mssecurity.mssecurity.Repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/role-permission")
+
 public class RolePermissionsController {
+    @Autowired
+    //Permite crear la entidad en la base de datos
+    private RolePermissionRepository theRolePermissionRepository;
+
+    // Referencia al repositorio de Role y Permission para poder buscar el rol y el permiso en la BD
+    @Autowired
+    private RoleRepository theRoleRepository;
+
+    @Autowired
+    private PermissionRepository thePermissionRepository;
+
+    // Método POST
+    // Para poder crear la relacion necesito mandar los id de las clases referenciadas en esta clase intermedia (se pueden mandar en la URL o en el body, en este caso en la URL)
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("role/{role_id}/permission/{permission_id}")
+    public RolePermission store(@PathVariable String role_id,
+                                @PathVariable String permission_id) {
+        // Buscamos el Rol y el permiso en la BD con su identificador role_id y permission_id
+        Role theRole=theRoleRepository.findById(role_id).orElse(null);
+        Permission thePermission=thePermissionRepository.findById(permission_id).orElse(null);
+        // Comprobamos que si existen
+        if(theRole!=null && thePermission!=null){
+            // Creamos el RolePermission con el constructor vacío.
+            RolePermission newRolePermission = new RolePermission();
+            // Le seteamos los valores al objeto ya que no lo hicimos en el cosntructor
+            newRolePermission.setRole(theRole);
+            newRolePermission.setPermission(thePermission);
+            // Guardamos en la DB
+            return this.theRolePermissionRepository.save(newRolePermission);
+        } else {
+            return null;
+        }
+    }
+    // Método GET (TODOS)
+    @GetMapping("")
+    public List<RolePermission> index(){
+        return this.theRolePermissionRepository.findAll();
+    }
+
+    // Método DELETE
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void destroy(@PathVariable String id) {
+        RolePermission theRoleRolePermission = this.theRolePermissionRepository
+                .findById(id)
+                .orElse(null);
+        if (theRoleRolePermission != null) {
+            this.theRolePermissionRepository.delete(theRoleRolePermission);
+        }
+    }
+
+
 }
--- src/main/java/com/mssecurity/mssecurity/Models/RolePermission.java
+++ src/main/java/com/mssecurity/mssecurity/Models/RolePermission.java
@@ -1,5 +1,33 @@
 package com.mssecurity.mssecurity.Models;
 
-public class RolePermission {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
+public class RolePermission {
+    @Id
+    // El guión bajo es debido a que así son los identificadores por defecto
+    private String _id;
+    @DBRef
+    private Role role;
+    @DBRef
+    private Permission permission;
+
+    public RolePermission() {
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
 }
